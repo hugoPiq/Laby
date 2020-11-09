d76f8c8197a546466ec935f954fe09799c813d49
hugoPiq <piquardhugo@yahoo.fr>
2020-11-09 08:29:44 +0100

    start

---
+++ Cell.h
@@ -0,0 +1,29 @@
+#ifndef __CELL_H__
+#define __CELL_H__
+
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+class Cell
+{
+  public:
+    Cell(int x, int y);
+    ~Cell();
+
+    void add_neighb(Cell *n1);
+    void add_neighb(Cell *n1, Cell *n2);
+    void add_neighb(Cell *n1, Cell *n2, Cell *n3);
+    friend ostream& operator<<(ostream& stream, const Cell& r);
+    friend istream& operator>>(istream& stream, Cell& r);
+  //protected:
+
+    int m_x, m_y; //pas const pour la lecture fichier
+    int m_nb_neighb = 0;
+    Cell **m_neighb = NULL;
+    bool m_displayed = false;
+    bool m_flag = false;
+    bool m_serial = false;
+};
+
+#endif
